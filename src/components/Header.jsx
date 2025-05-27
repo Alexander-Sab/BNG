b2257443a7f5b1f2a84352d04ae9f0702cd38a34
Alexander-Sab <shura.saprykin.88@mail.ru>
2025-05-27 15:15:07 +0300

    кнопка о работе

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FaArrowLeft } from 'react-icons/fa'; // Убедитесь, что этот импорт корректен
-import icon from './sluzhba_final.svg';
+import { FaInfoCircle } from 'react-icons/fa';
 import './Header.css';
 
 export default function Header() {
@@ -9,15 +7,17 @@ export default function Header() {
   
   return (
     <div className="about-page">
-      {/* Добавляем кнопку "Назад" вверху страницы */}
-      <div className="about-header">
-       <button className="button-icon-slugba" onClick={() => navigate(-1)}>
-  <img className="icon-slugba" src={icon} alt="Назад"  />
-</button>
-        {/* <h1>О нашей работе</h1> */}
-      </div>
-      
       <header className="header">
+        {/* Кнопка теперь внутри .header но позиционируется абсолютно */}
+        <button 
+          className="about-button"
+          onClick={() => navigate('/about')}
+          aria-label="Подробно о нашей работе"
+        >
+          <FaInfoCircle />
+          <span>О работе</span>
+        </button>
+
         <h1>Федеральная Служба ВНГ РФ</h1>
         <p className="header-p1">приглашает на работу<br /> в городе Москве:</p>
         <p className="header-p2">полицейских, полицейских (водителей)</p>
